46e7acdfdd74bfd9cac877cf8c5a48e9331ce0b7
om-prakash-yadav <jnvomprakash121@gmail.com>
2025-07-28 15:19:08 +0530

    removed additional settings to work with new appproach

--- src/components/templates/additional-settings-section/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AdditionalSettingsSection } from './additional-settings-section';
-export type { AdditionalSettingsSectionRef } from './additional-settings-section-types';
-export * from './additional-settings-section-constants';
